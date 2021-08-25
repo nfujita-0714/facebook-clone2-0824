353527cc8c14e9298460a0648e798e8700d4f02c
nfujita0707 <nfujita0714@gmail.com>
2021-08-25 02:19:49 +0000

    ルーディング再設定・link_toの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :pictures do
     collection do
-     post :confirm
+    post :confirm
     end
   end
   resources :sessions, only: [:new, :create, :destroy]
+  resources :users
   resources :users, only: [:new, :create, :show]
 end
